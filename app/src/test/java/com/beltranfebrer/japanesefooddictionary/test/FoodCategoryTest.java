97533f5de6ffbc66af4faeec965cbdf10d44a8b8
Miquel Beltran <mike513@gmail.com>
2016-01-13 12:36:32 +0100

    Added test for Food Categories

---
+++ app/src/test/java/com/beltranfebrer/japanesefooddictionary/test/FoodCategoryTest.java
@@ -0,0 +1,40 @@
+package com.beltranfebrer.japanesefooddictionary.test;
+
+import com.beltranfebrer.japanesefooddictionary.data.FoodCategory;
+import com.beltranfebrer.japanesefooddictionary.data.FoodDescription;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static junit.framework.Assert.assertEquals;
+
+/**
+ * Created by miquel on 13.01.16.
+ */
+public class FoodCategoryTest {
+    private static final String NAME = "Name";
+    private static final String ICON = "Icon";
+    private static final String TO_STRING = NAME + " (1)";
+    private static final String CATEGORY = "Cat";
+    private static final String ENGLISH = "English";
+    private static final String JAPANESE = "Japanese";
+    private static final String PRONUNCIATION = "Pro";
+    FoodCategory category;
+
+    @Before
+    public void setUp() throws Exception {
+        category = new FoodCategory(NAME, ICON);
+        category.addFoodDescription(new FoodDescription(CATEGORY, ENGLISH, JAPANESE, PRONUNCIATION));
+    }
+
+    @Test
+    public void testToString() throws Exception {
+        assertEquals(TO_STRING, category.toString());
+    }
+
+    @Test
+    public void testGetList() throws Exception {
+        assertEquals(1,category.getFoodDescriptionList().size());
+        assertEquals(ENGLISH, category.getFoodDescriptionList().get(0).english);
+    }
+}
